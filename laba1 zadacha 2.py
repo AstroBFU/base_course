2bc23bfce3bc0137a288e7e6186bf8dd680d6ca4
danya-codit <66479573+danya-codit@users.noreply.github.com>
2020-06-10 18:19:43 +0200

    Add files via upload

---
+++ laba1 zadacha 2.py
@@ -0,0 +1,14 @@
+# Определите типы следующих объектов:
+
+# х = ‘Hello, World!’
+# х = 3 + 4
+# х = 3 / 4
+# х = [1, 2, 5, 10, 100]
+x = "hello world"
+print(type(x))
+x = 3 + 4
+print(type(x))
+x = 3 / 4
+print(type(x))
+x = [1, 2, 5, 10, 100]
+print(type(x))

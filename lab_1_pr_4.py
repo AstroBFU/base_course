f989852788fe5a8bb2916423c96e27206e57a7ac
viktorod <66479433+viktorod@users.noreply.github.com>
2020-06-12 12:47:36 +0200

    Add files via upload

---
+++ lab_1_pr_4.py
@@ -0,0 +1,39 @@
+a = [1, 5, 'Good', 'Bad']
+
+b = [9, 'Blue', 'Red', 11]
+
+c = a[1] + b[3]
+
+print(c) 
+
+a = [1, 5, 'Good', 'Bad']
+
+b = [9, 'Blue', 'Red', 11]
+
+c = a[2] + b[2]
+
+print(c) 
+
+a = [1, 5, 'Good', 'Bad']
+
+b = [9, 'Blue', 'Red', 11]
+
+c = a[0] * b[0]
+
+print(c) 
+
+a = [1, 5, 'Good', 'Bad']
+
+b = [9, 'Blue', 'Red', 11]
+
+c = a[1] ** b[3]
+
+print(c) 
+
+a = [1, 5, 'Good', 'Bad']
+
+b = [9, 'Blue', 'Red', 11]
+
+c = a + b
+
+print(c) 

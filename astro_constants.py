9ef86ec03915bff7e5c1d727da92b977c0b1d1b7
danya-codit <66479573+danya-codit@users.noreply.github.com>
2020-07-06 11:39:25 +0200

    Add files via upload

---
+++ astro_constants.py
@@ -0,0 +1,17 @@
+# ______________________astroconstants__________________________
+
+ears_mass = 5.94 * 10 ** 24 # kg
+graviti_constant = 6.672 * 10**-11 # m**3 * kg**-1 * c**-2
+spid_sveta = 2.998*10**8 # m/c 
+constant_boltsmana = 1.38 * 10**-23 # m**2 * kg * c**-2 *k**-1
+universal_gaz_constant = 8.31 # m**2 * kg * c**-2 * k**-1 * mol**-1
+constant_stefana_boltsmana = 5.67 * 10**-8 # kg * c**-3 * k**-4
+constant_planka = 6.626 * 10**-34 # kg * m**2 *c**-1
+massa_protona = 1.67 * 10**-27 # kg
+massa_electrona = 9.11 * 10**-31 # kg
+elementary_zaryad = 1.602 * 10**-19 # kl
+astronomy_edenytsa = 1.496 * 10**11 # m
+parsek = 3.086 * 10**16 # m
+constant_habla = 68000 # m / c
+acceleration_of_gravity_ears = 9.806 # м * с**-2
+chislo_elera = 2.7182818284

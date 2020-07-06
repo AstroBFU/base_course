3a43f7e47e7dde682ad69169ccc2537654f343b7
danya-codit <66479573+danya-codit@users.noreply.github.com>
2020-07-06 11:38:12 +0200

    Delete laba 2 zadacha 3.py

--- laba 2 zadacha 3.py
+++
@@ -1,6 +0,0 @@
-god = int (input ())
-print (god)
-if  god % 4 == 0  and  god % 100 != 0    or    god % 4 == 0 and  god % 400 == 0: 
-    print ( " eto visokosni ")
-else:
-    print ( " eto ne visokosni ")

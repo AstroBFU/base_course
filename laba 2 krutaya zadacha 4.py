88262118c0cfbfdfbc5c9daff5e2b73ed2307fd8
danya-codit <66479573+danya-codit@users.noreply.github.com>
2020-07-06 11:38:21 +0200

    Delete laba 2 krutaya zadacha 4.py

--- laba 2 krutaya zadacha 4.py
+++
@@ -1,9 +0,0 @@
-print ( " vvedi chislo  " )
-chislo = int (input ())
-print ( " chislo =" , chislo )
-m = 0
-print ( " perevernutoe chislo:")
-while chislo>0:
-    m = m*10 + chislo%10
-    chislo = chislo//10
-print(m)

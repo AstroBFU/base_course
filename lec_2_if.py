3cee05f2cf57e7212762f067e109f6a119e60bde
userdimon <72092995+userdimon@users.noreply.github.com>
2020-09-29 17:59:20 +0200

    сделал лекцию 2

---
+++ lec_2_if.py
@@ -0,0 +1,6 @@
+a=int(input('введите число_'))
+    
+print(a)
+
+if a>1:
+    print('hello',a)
---
+++ lec_2_if_elif_else.py
@@ -0,0 +1,7 @@
+a=int(input('введите число_'))
+if a>5:
+    print('hello 5')
+elif a<3:
+    print('hello 2')
+else:
+    print('Tupo hello')

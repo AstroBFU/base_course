0e8b4cfe228cb70fc6df4e035b501442ac1f3b30
gkhlevtov <71844829+gkhlevtov@users.noreply.github.com>
2020-10-01 18:01:20 +0200

    Delete lab_ex5.py

--- lab_ex5.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split())
-if b == 0:
-    print('Ошибка')
-elif a % b == 0:
-    print('Число', a, 'делится на число', b)
-    print(a % b)
-    print(a, '/', b, '=', int(a / b))
-else:
-    print(a, '/', b, '=', a / b)

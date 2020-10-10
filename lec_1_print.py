ea54bdbfcc9637cf41fdeb1c94b13351cc8ac8f3
thesalem-cot <72652748+thesalem-cot@users.noreply.github.com>
2020-10-10 14:03:46 +0200

    Add files via upload
    123

---
+++ lec_1_print.py
@@ -0,0 +1,5 @@
+print()
+print(3) 
+print(3+4)
+print(3, 4)
+
---
+++ lec_1_tip.py
@@ -0,0 +1,17 @@
+type #Команда определения типа данных
+
+type (3)
+print (type(3))
+
+type (3.4)
+print (type(3.4))
+
+type ("Молодец")
+print (type("Молодец"))
+
+a= "Молодец"
+print (a)
+
+type ({1,2,3})
+print ({1,2,3})
+

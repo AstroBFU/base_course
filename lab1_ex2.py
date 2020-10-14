7e909391da165bc2a594a9219c92bfc84d1bf5f7
Sergei8888 <45688549+Sergei8888@users.noreply.github.com>
2020-10-14 19:49:12 +0200

    awd
    qwdqwd

---
+++ lab1_ex2.py
@@ -0,0 +1,3 @@
+n_1, q, ns = map(int, input().split())
+for i in range(ns):
+    print(n_1 * q**i)
---
+++ lab2_ex6.py
@@ -0,0 +1,4 @@
+for i in range(1, 10):
+    print("")
+    for k in range(1, 10):
+         print(1 * (i * k), end = " ")

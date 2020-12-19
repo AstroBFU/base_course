d7d5e3912b93a160beb351e8c94bfb60962d2e2c
dimadima1159 <72620312+dimadima1159@users.noreply.github.com>
2020-12-19 14:53:22 +0200

    Update lec_6_beauty.py

--- lec_6_beauty.py
+++ lec_6_beauty.py
@@ -1,12 +1,13 @@
-import mathlotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
-x = [3, 4, 5]
-y = [40, 10, 30]
 
-plt.plot(x, y, color='g', label='luchte', marker='^', ms=5)
-plt.xlabel('coord: x')
-plt.ylabel('coord: y')
-plt.legende()
+x = [3,4,5]
+y = [40,10,30]
+plt.plot(x,y, color='g', label='lunchte', marker='>',ms=5)
+#----Украшательства----
+plt.xlabel('Coord: x')
+plt.ylabel('Coord: y')
+plt.legend()
 plt.title('Base')
 plt.grid()
 plt.show()

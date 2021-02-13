36b2f9c72e4b8a4899909a399e72d0c29ca87550
dimadima1159 <72620312+dimadima1159@users.noreply.github.com>
2021-02-13 13:22:11 +0200

    Create lec_7_non_standart_obj.py

---
+++ lec_7_non_standart_obj.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots()
+ball, = plt.plot([], [], 'o', color='r', label='Ball')
+
+def circle_move(R, vx0, vy0, time):
+  x0 = vx0 * time
+  y0 = vy0 * time
+  alpha = np.arange(0, 2*np.pi, 0.1)
+  x = x0 + R*np.cos(alpha)
+  y = y0 + R*np.sin(alpha)
+  return x, y
+
+edge = 3
+plt.axis('equal')
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
+
+def animate(i):
+  ball.set_data(circle_move(R=0.5, vx0=0.01, vy0=0.01, time=i))
+ani = FuncAnimation(fig,
+animate,
+frames=100,
+interval=30
+)
+ani.save('lec_7_complex_animation.gif')

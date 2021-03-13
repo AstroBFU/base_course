6c8602603e9da4ed80cf8ee7fb7fc369d9fb1518
y02DSS <za02za02@bk.ru>
2021-03-13 15:13:41 +0000

    New words

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,5 @@
 import time
 a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z']
-for i in range (24):
+for i in range (20):
 	print (a[i])
 

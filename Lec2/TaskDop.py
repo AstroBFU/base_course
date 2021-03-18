0cae23824d2236e47a5a39a832af5db5997f51e3
IgorSivirinov <54248137+IgorSivirinov@users.noreply.github.com>
2021-03-18 16:34:34 +0200

    Delete Lec2 directory

--- Lec2/TaskDop.py
+++
@@ -1,7 +0,0 @@
-import math
-G = 9.8
-H = 100
-time = float(input('Время в секундах: '))
-l = ((G**2 * time**2) / (H - 1)) * (4 * math.pi - 5 * G**(3/2)) - 10
-
-print(l)
--- Lec2/if_elif_else.py
+++
@@ -1,7 +0,0 @@
-import math
-a = float(input())
-if math.sqrt(a)%1==0:
-    print('Ok')
-elif math.sqrt(a)==0:
-    print('Norm')
-else: print("Not")

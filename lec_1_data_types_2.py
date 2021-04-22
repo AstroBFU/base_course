23cdb0ae098cf615bcc4a679a1d2f7f31cd95007
Saga0001 <>
2021-04-22 15:21:49 +0000

    done

---
+++ lec_1_data_types_2.py
@@ -0,0 +1,5 @@
+print (type(True))
+
+print(type(False))
+
+print(type(None))
---
+++ lec_1_working_with_strings.py
@@ -0,0 +1,13 @@
+a='Good'
+b='Bad'
+
+print(a + b+b)
+
+
+print(a * 3)
+
+c=50
+d=10
+print(f'Вставим числа {c} и {d}')
+
+print(len(a+b))

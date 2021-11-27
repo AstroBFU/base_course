b45b6a47fd5dd3f87087375e202ec8118808bf49
sodasof52 <92211875+sodasof52@users.noreply.github.com>
2021-11-27 11:58:36 +0000

    какие-то треугольники

---
+++ norm_name.py
@@ -0,0 +1,13 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+if a + b <= c or a + c <= b or b + c <= a:
+  print("Не существует") 
+elif a == b and b == c:
+  print("Равносторонний")
+elif a == c or a == b:
+  print("Равнобедренный")
+elif a != b and b != c:
+  print("Разносторонний")
+else: 
+  print("Существует")

5395d65c522f1d1c48d77ee0c9a7f4f077ca4329
annamukharyamov <114501284+annamukharyamova@users.noreply.github.com>
2022-10-11 16:48:21 +0000

    if

---
+++ lec_2_if.py
@@ -0,0 +1,11 @@
+if 0:
+  print("hello 1")
+
+a = 3
+if a > 1:
+  print(f"hello {a}")
+
+b = 5
+if b == 5:
+  print(f"hello {b}")
+
---
+++ lec_2_if_else.py
@@ -0,0 +1,6 @@
+a = 3
+if a > 4:
+  print("hello 4")
+else:
+  print(f"hello {a}")
+        

7fcb0ec1c256680fa361a4ebcf81df7e6d095333
annamukharyamov <114501284+annamukharyamova@users.noreply.github.com>
2022-11-29 15:58:58 +0000

    tttt

---
+++ lec_3_np.array.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+h = 100
+g = 9.8
+c = g * h * np.tan(B)**2
+b = 2 * np.cos(a)**2 * (1 - np.tan(B) * np.tan(a) )
---
+++ lec_3_slice.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a = [0, 5, 3, 6]
+slise = a[0:2:1]
+print(slise)

d76b4ff19ce8b680d5b1dc97c9d19a9980324a81
eruntomn <114501121+eruntomn@users.noreply.github.com>
2022-12-06 14:22:55 +0000

    06.12

--- lab_4_task_5.py
+++ lab_4_task_5.py
@@ -1,2 +1,18 @@
-def chek(t):
-    
+import numpy as np
+'''print('прямоугольник/треугольник/круг')
+a = input()
+s = 1
+if a == 'прямоугольник':
+    def chek(a, b):
+        s = a * b
+        print(s)
+elif a =='треугольник':
+    def chek(a, h):
+        s = 0.5 * a * h
+        print(s)
+else:
+    def chek(r):
+        s = np.pi*r**2
+        print(s)'''
+
+

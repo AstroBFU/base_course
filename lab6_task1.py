aabaa06b87e838d9621446a4704c386e5d82cfb2
xkotoff <114501069+xkotoff@users.noreply.github.com>
2022-12-13 17:00:46 +0000

    здравый код

--- lab6_task1.py
+++ lab6_task1.py
@@ -1,13 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.plot([1.1, 5.1], [1.5, 5.5])
-
-plt.xlabel('Coord: x') 
-plt.ylabel('Coord: y')
-plt.legend()
-plt.title('Base')
-plt.grid()
+plt.plot([1,1 1,5 5,1 5,5] [5,1 5,5 1,1 1,5 ])
 plt.savefig('pic_1.png')
 
 

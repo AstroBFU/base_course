c50c3bcd5f5dea53f000ecc3e6b7b9c74efbdf05
jauxusj <144795511+jauxusj@users.noreply.github.com>
2023-09-19 14:39:17 +0200

    u7yiyt7

---
+++ lec_1_datatypes.py
@@ -0,0 +1,19 @@
+type
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type("Молодец"))
+
+a="Molodec"
+print(type(a))
+
+a=['Molodec', 1, 'Курто', 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(None))
---
+++ lec_1_input.py
@@ -0,0 +1,10 @@
+a=input()
+print(a)
+
+a=input('ВВедите значение a: ')
+print(a)
+print(type(a))
+
+a=int(input('ВВедите значение a: '))
+print(a)
+print(type(a))

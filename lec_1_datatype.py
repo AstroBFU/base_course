236a3e7f6e3b8505c7fa68e45ae1088f85e795af
grinyyaaa <144795933+grinyyaaa@users.noreply.github.com>
2023-10-03 00:29:45 +0200

    lab1_2_main

---
+++ lec_1_datatype.py
@@ -0,0 +1,21 @@
+type # Команда определения типа данных
+
+type(3)
+
+print(type(3))
+
+print(type(3.4))
+
+print(type('Молодец'))
+
+a = 'Molodec'
+print(type(a))
+
+a = ['Molodec', 1, 'Курто', 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))

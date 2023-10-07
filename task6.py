c208ddc42c5259a17207362914f8ea992da7b049
stu8pi <146557595+stu8pi@users.noreply.github.com>
2023-10-07 16:07:24 +0000

    Первая лабораторная

---
+++ task6.py
@@ -0,0 +1,7 @@
+a = []
+a.append(input('Введите Ваш возраст: '))
+a.append(input('Введите Ваш пол: '))
+a.append(input('Введите Ваше имя: '))
+a.append(input('Введите Ваш город: '))
+a.append(input('Введите Ваш класс: '))
+print(a)

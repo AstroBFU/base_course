dbbc3035e75bc2ad0da51387346097a9dda35045
IlyuhaShishka <104719710+IlyuhaShishka@users.noreply.github.com>
2023-10-20 20:46:48 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,8 +1,10 @@
-from math import sqrt
-x = 3
+a = [1, 5, 'Good', 'Bad']
+b = [9, 'Blue', 'Red', 11]
 
-y1 = (sqrt(x**3) / (x ** 3 + 3 / x)) * (4 * x ** 7 - x ** 5) + 80 * sqrt(27 * x ** 4 + 12 * x ** 3 - 5 * x ** 2 + 10)
-print(y1)
+print(a[1] + b[3])
+print(a[2] + b[2])
+print(a[0] * b[0])
+print(a[1] ** b[3])
 
-y2 = ((3 % 2) + int(16.7 * 4.32)) / (14.5 + (31 % 12) - int(x ** 3.4))
-print(y2)
+c = a + b
+print(c)
